3a2c25bf942d8a075f89b1590939390e1f6ef847
cmontella <cmontella@live.com>
2021-05-02 22:58:48 -0400

    Add a core field to mech/compile

--- src/compile.rs
+++ src/compile.rs
@@ -8,6 +8,7 @@ use std::collections::HashMap;
 
 lazy_static! {
   static ref MECH_COMPILE: u64 = hash_string("mech/compile");
+  static ref CORE: u64 = hash_string("core");
   static ref CODE: u64 = hash_string("code");
 }
 
@@ -15,7 +16,7 @@ export_machine!(mech_compile, mech_compile_reg);
 
 extern "C" fn mech_compile_reg(registrar: &mut dyn MachineRegistrar, outgoing: Sender<RunLoopMessage>) -> String {
   registrar.register_machine(Box::new(Compile{outgoing}));
-  "#mech/compile = [|code|]".to_string()
+  "#mech/compile = [|core code|]".to_string()
 }
 
 #[derive(Debug)]
@@ -35,11 +36,13 @@ impl Machine for Compile {
 
   fn on_change(&mut self, table: &Table) -> Result<(), String> {
     for i in 1..=table.rows {
+      let core = table.get_u64(&TableIndex::Index(i), &TableIndex::Alias(*CORE));
       let code = table.get_string(&TableIndex::Index(i), &TableIndex::Alias(*CODE));
-      match code {
-        Some((code,_)) => {
+
+      match (core,code) {
+        (Some((core,_)),Some((code,_))) => {
           let outgoing = self.outgoing.clone();
-          outgoing.send(RunLoopMessage::Code((0,MechCode::String(code.to_string()))));
+          outgoing.send(RunLoopMessage::Code((core,MechCode::String(code.to_string()))));
         }
         _ => (), // TODO Send error
       }
